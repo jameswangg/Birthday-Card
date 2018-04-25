f5b21b21c579535b2234f079cdc94dacdf789eb9
jameswangg <37317180+jameswangg@users.noreply.github.com>
2018-04-25 11:04:45 +0900

    Add files via upload

---
+++ GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayCard.Models
+{
+    public class GuestResponse
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Message { get; set; }
+
+    }
+}
